aefe263ff9a2520b37970044ace7ab55ed138e13
OseiAlex <34588600+OseiAlex@users.noreply.github.com>
2024-09-18 13:40:22 +0000

    Did the grade calculator assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        String valueFromUser = Console.ReadLine();
+        int percent = int.Parse(valueFromUser);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        {
+            
+            if (percent >= 70)
+            {
+                Console.WriteLine("You Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Better luck next time!");
+            }
+        }
+       /*int x = int.Parse(valueFromUser);
+       int y = 2;
+
+       if (x > y)
+       {
+        Console.WriteLine("x is greater than y");
+       }
+       else if (x < y)
+       {
+        Console.WriteLine("x is less than y");
+       }
+       else
+       {
+        Console.WriteLine("Equal");
+       }*/
     }
 }
